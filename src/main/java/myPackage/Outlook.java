c45ba788ea9c061e3d416ac3b1b5ca2d69eb0abc
danerwilliams <danerwilliams@gmail.com>
2019-01-17 16:55:09 -0500

    Update Outlook.java

--- src/main/java/myPackage/Outlook.java
+++ src/main/java/myPackage/Outlook.java
@@ -19,7 +19,12 @@ public class Outlook {
 
     public void CreateCalendarEvent(String text) throws UnirestException, FileNotFoundException {
         ParseForm pf = new ParseForm(text);
-        String body = "{\"Subject\":\"" + pf.getSubject() + "\"," + "\"Body\": {" + "\"ContentType\": \"HTML\"," + "\"Content\":" + "\"" + pf.getMessage() + "\"" + "}," + "\"Start\": {" + "\"DateTime\":" + "\"" + pf.reformatDate() + pf.getStartTime() + "\"," + "\"TimeZone\": \"Pacific Standard Time\"" + "}," + "\"End\": {" + "\"DateTime\":" + "\"" + pf.reformatDate() + pf.getEndTime() + "\"," + "\"TimeZone\": \"Pacific Standard Time\"" + "}," + "\"Attendees\": [" + "{" + "\"EmailAddress\": {" + "\"Address\": \"TestAcc@eastsidecatholic.org\"" + "}," + "\"Type\": \"Required\"" + "}," + "{" + "\"EmailAddress\": {" + "\"Address\":" + "\"" + pf.getTeacherEmail() + "\"" + "}," + "\"Type\": \"Required\"" + "}" + "]" + "}";
+        String body = "{\"Subject\":\"" + pf.getSubject() + "\"," + "\"Body\": {" + "\"ContentType\": \"HTML\"," + "\"Content\":" + "\"" + 
+            pf.getMessage() + "\"" + "}," + "\"Start\": {" + "\"DateTime\":" + "\"" + pf.reformatDate() + pf.getStartTime() + 
+            "\"," + "\"TimeZone\": \"Pacific Standard Time\"" + "}," + "\"End\": {" + "\"DateTime\":" + "\"" + 
+            pf.reformatDate() + pf.getEndTime() + "\"," + "\"TimeZone\": \"Pacific Standard Time\"" + "}," + "\"Attendees\": [" + 
+            "{" + "\"EmailAddress\": {" + "\"Address\": \"TestAcc@eastsidecatholic.org\"" + "}," + "\"Type\": \"Required\"" + "}," + 
+            "{" + "\"EmailAddress\": {" + "\"Address\":" + "\"" + pf.getTeacherEmail() + "\"" + "}," + "\"Type\": \"Required\"" + "}" + "]" + "}";
         System.out.println(body);
         HttpResponse StringResponse = Unirest.post((String)"https://outlook.office.com/api/v2.0/me/events").header("Accept", "text/*, application/xml, application/json; odata.metadata=none").header("Authorization", "Bearer " + this.access_token).header("Content-Type", "application/json; charset=utf-8").body(body).asString();
         System.out.println(StringResponse.getStatusText());
